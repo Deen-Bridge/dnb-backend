69e5edb2cbca8ff63c66fae74a15e332f05be096
Sakariyah Abdulhazeem <sakariyahabdulhazeem@gmail.com>
2025-04-28 23:10:36 +0100

    Initial commit for backend

---
+++ app.js
@@ -0,0 +1,51 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+import { Server as SocketIOServer } from "socket.io";
+import http from "http";
+
+// Load env
+import dotenv from "dotenv";
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+const server = http.createServer(app);
+const io = new SocketIOServer(server, {
+  cors: { origin: "*" },
+});
+
+// Middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cookieParser());
+
+// TODO: Mount your routes here, e.g.:
+// app.use('/api/auth', authRoutes);
+// app.use('/api/messages', messageRoutes);
+
+// Socket.io for real-time messaging
+io.on("connection", (socket) => {
+  console.log("⚡ New WebSocket connection:", socket.id);
+
+  socket.on("joinRoom", ({ conversationId }) => {
+    socket.join(conversationId);
+  });
+
+  socket.on("sendMessage", (data) => {
+    io.to(data.conversationId).emit("receiveMessage", data);
+    // TODO: Persist message in DB
+  });
+
+  socket.on("disconnect", () => {
+    console.log("🛑 WebSocket disconnected:", socket.id);
+  });
+});
+
+export { server }; // so we can start it with HTTP + Socket.io
+export default app;
